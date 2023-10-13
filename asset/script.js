3985ceb8d7b9c5b9851cc11db911a3d72df84ca3
mori-akira <77237845+mori-akira@users.noreply.github.com>
2023-10-13 18:54:38 +0900

    update

--- asset/script.js
+++ asset/script.js
@@ -1,9 +1,34 @@
-document.querySelectorAll('mask-block, m-b').forEach(e => {
-    e.addEventListener('click', () => {
-        if (e.classList.contains('clicked')) {
-            e.removeAttribute('class');
-        } else {
-            e.setAttribute('class', 'clicked');
+$(() => {
+    $('mask-block, m-b').each((i, e) => {
+        $(e).click(() => {
+            if ($(e).hasClass('clicked')) {
+                $(e).removeClass('clicked');
+            } else {
+                $(e).addClass('clicked');
+            }
+        });
+    });
+
+    let details = undefined;
+    let ul = undefined;
+    $('h2, h3').each((i, e) => {
+        const id = `header-${i}`;
+        $(e).attr('id', id);
+        if ('H2' === $(e).prop('tagName')) {
+            if (details) {
+                details.append(ul);
+                $('#page-tree').append(details);
+            }
+            details = $(`<details><summary><a href="#${id}">${$(e).text()}</summary></details>`);
+            ul = $('<ul></ul>')
+        } else if ('H3' === $(e).prop('tagName')) {
+            if (ul) {
+                ul.append($(`<li><a href="#${id}">${$(e).text()}</a></li>`));
+            }
         }
     });
+    if (details) {
+        details.append(ul);
+        $('#page-tree').append(details);
+    }
 });
